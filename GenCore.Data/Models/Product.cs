0c8a6ff1e658f111b941d8f91a2f67c5e69c0f3c
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-03 14:35:26 +1100

    Add ProductRepository

---
+++ GenCore.Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using GenCore.Data.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCore.Data.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+
+        //max 100 characters
+        public string Name { get; set; }
+
+        //2 decimals
+        public decimal Price { get; set; }
+
+        public ProductTypeEnum Type { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ GenCore.Data/Repositories/Implementation/ProductRepository.cs
@@ -0,0 +1,158 @@
+﻿using Dapper;
+using GenCore.Data.Models;
+using GenCore.Data.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCore.Data.Repositories.Implementation
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly string _connectionString;
+
+        public ProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"SELECT 
+                                        p.ProductId,
+	                                    p.Name, 
+	                                    p.Price, 
+	                                    p.Type, 
+	                                    p.Active
+                                    FROM 
+	                                    production.products p";
+                    var result = connection.Query<Product>(sql);
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return new List<Product>();
+            }
+        }
+
+        public int Insert(Product product)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"INSERT INTO production.products
+                                    (                    
+	                                    Name,
+	                                    Price,
+	                                    Type,
+	                                    Active
+                                    )
+                                    VALUES 
+                                    ( 
+	                                    @Name,
+	                                    @Price,
+	                                    @Type,
+	                                    @Active
+                                    )";
+                    var result = connection.Execute(sql, new
+                    {
+                        Name = product.Name,
+                        Price = product.Price,
+                        Type = product.Type.ToString(),
+                        Active = product.Active
+                    });
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        public int Update(long id, string product)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string updateSql = _sqlConverter.ToSql(product);
+
+                    if (updateSql.IsEmpty())
+                    {
+                        return 0;
+                    }
+
+                    connection.Open();
+
+                    string sql = $@"UPDATE
+	                                production.products
+                                SET
+	                                {updateSql}
+                                WHERE
+                                    ProductId = @Id";
+                    var result = connection.Execute(sql, new
+                    {
+                        Id = id
+                    });
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        public int Delete(long id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"DELETE FROM
+	                                production.products
+                                WHERE
+	                                ProductId = @Id";
+                    var result = connection.Execute(sql, new
+                    {
+                        Id = id
+                    });
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}

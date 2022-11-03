bbba33de1f10c44c549ece9e24d4436933406554
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-03 17:11:11 +1100

    Add create table method to UserRepository

--- GenCore.Data/Repositories/Implementation/UserRepository.cs
+++ GenCore.Data/Repositories/Implementation/UserRepository.cs
@@ -18,6 +18,70 @@ namespace GenCore.Data.Repositories.Implementation
         public UserRepository(string connectionString)
         {
             _connectionString = connectionString;
+            CreateTable();
+        }
+
+        private int CreateTable()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"IF 
+	                                    (NOT EXISTS (SELECT TABLE_CATALOG FROM INFORMATION_SCHEMA.TABLES  
+                                                        WHERE TABLE_SCHEMA = 'auth' 
+                                                        AND  TABLE_NAME = 'users'))
+                                    BEGIN
+                                        CREATE TABLE auth.users (
+											UserId BIGINT IDENTITY (1, 1) PRIMARY KEY,
+											Username NVARCHAR (100) NOT NULL CHECK (LEN(Username) > 4),
+											Email NVARCHAR (100) NOT NULL,
+											Phone NVARCHAR (20) NOT NULL,
+											LastActive DATETIME NOT NULL CHECK (LastActive < GETDATE()),
+											UNIQUE(Username)
+										)
+                                    END";
+
+                    var result = connection.Execute(sql);
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        private int DropTable()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"DROP TABLE IF EXISTS auth.userencryption
+                                    DROP TABLE IF EXISTS auth.usertokens
+                                    DROP TABLE IF EXISTS auth.userroles
+                                    DROP TABLE IF EXISTS auth.userverification
+                                    DROP TABLE IF EXISTS auth.users";
+
+                    var result = connection.Execute(sql);
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
         }
 
         public long Insert(User user)

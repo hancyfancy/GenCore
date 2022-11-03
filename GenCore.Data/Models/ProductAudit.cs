d0a3f33bd802ad6b7150c296304006a8968638be
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-03 14:23:32 +1100

    Add ProductAuditRepository

---
+++ GenCore.Data/Models/ProductAudit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCore.Data.Models
+{
+    public class ProductAudit
+    {
+        public long ProductAuditId { get; set; }
+
+        public string ObjJson { get; set; }
+
+        public DateTime AuditDateTime { get; set; }
+    }
+}
--- GenCore.Data/Repositories/Implementation/ProductAuditRepository.cs
+++ GenCore.Data/Repositories/Implementation/ProductAuditRepository.cs
@@ -1,6 +1,10 @@
-﻿using GenCore.Data.Repositories.Interface;
+﻿using Dapper;
+using GenCore.Data.Models;
+using GenCore.Data.Repositories.Interface;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,46 @@ namespace GenCore.Data.Repositories.Implementation
 {
     public class ProductAuditRepository : IProductAuditRepository
     {
+        private readonly string _connectionString;
+
+        public ProductAuditRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<ProductAudit> GetUpdates(long productId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"SELECT 
+                                    p.ProductAuditId,
+	                                p.ObjJson,
+	                                p.AuditDateTime
+                                FROM 
+	                                audit.products p
+                                WHERE
+	                                p.EventType = 'UPDATE'
+									AND p.ProductId = @ProductId
+								ORDER BY 
+									p.AuditDateTime";
+                    var result = connection.Query<ProductAudit>(sql, new
+                    {
+                        ProductId = productId
+                    });
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return new List<ProductAudit>();
+            }
+        }
     }
 }

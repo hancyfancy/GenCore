3e3fe61746825d91aff80e642864863e676be38f
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-04 11:24:05 +1100

    Create audit schema

---
+++ GenCore.Data/Schemas/Implementation/AuditSchema.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using GenCore.Data.Schemas.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCore.Data.Schemas.Implementation
+{
+    internal class AuditSchema : ConnectionBase, ISchema
+    {
+        public AuditSchema(string connectionString) : base(connectionString)
+        {
+            CreateSchema();
+        }
+
+        private int CreateSchema()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"USE {_database}
+
+                                    IF NOT EXISTS ( SELECT  schema_id
+                                                    FROM    sys.schemas
+                                                    WHERE object_id = OBJECT_ID(N'audit' )
+                                    BEGIN
+	                                    CREATE SCHEMA audit
+                                    END";
+
+                    var result = connection.Execute(sql);
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        private int DropSchema()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string sql = $@"USE {_database}
+
+                                    DROP SCHEMA IF EXISTS audit";
+
+                    var result = connection.Execute(sql);
+
+                    connection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
